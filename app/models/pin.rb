31ee09490421c468e729194b2372e1db1b385d96
Carleton Atwater <atwaterc@gmail.com>
2013-04-06 14:44:50 -0400

    Add Paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,10 +3,10 @@ class Pin < ActiveRecord::Base
 
   validates :description, presence: true
   validates :user_id, presence: true
-  validates_attachment :image, prescence: true,
-                        content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']}
-                                                size: {less_then: 5.megabytes }
+  validates_attachment :image, presence: true,
+                        content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']},
+                                                size: {less_than: 5.megabytes }
   belongs_to :user
-  has_attached_file :image
+  has_attached_file :image, styles: {medium: "320x240"}
 
 end
